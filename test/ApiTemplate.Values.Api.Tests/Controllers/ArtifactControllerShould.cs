252d844fb7fc06428bc4cb7785cb1455d38b9524
adria-arquimbau <adria.arquimbau@gmail.com>
2020-10-23 12:47:19 +0200

    test: artifact controller should

---
+++ test/ApiTemplate.Values.Api.Tests/Controllers/ArtifactControllerShould.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace ApiTemplate.Values.Api.Tests.Controllers
+{
+    public class ArtifactControllerShould : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public ArtifactControllerShould(  
+            CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {   
+                AllowAutoRedirect = false
+            });
+            Task.WaitAll(_factory.RespawnDbContext());
+        }
+    }
+}
